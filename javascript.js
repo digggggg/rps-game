a97cfba68436daba22974397925913726ae90bf1
C K <ctkoo@ucsc.edu>
2022-06-23 12:17:07 -0700

    final touches

--- javascript.js
+++ javascript.js
@@ -4,12 +4,28 @@ var ties = 0;
 
 var playerChoice;
 
-var computerChoice;
+function randomNo(x,y){
+    var min = x;
+    var max = y;
+    
+    var randomNo = Math.floor(Math.random()*(max-min+1)+min);
+    
+    return randomNo;
+}
+
+var choiceRPS = ["r", "p", "s"];
+
+var computerChoice = "who knows";
+
+var choiceIndex = 0;
 
 var keepPlaying = true;
 
 do {
-    playerChoice = prompt("Pick your choice: R, P, S");
+    playerChoice = prompt("Pick your choice: R, P, S").toLowerCase();
+    alert("You chose "+ playerChoice);
+    choiceIndex = randomNo(0,choiceRPS.length-1);
+    computerChoice = choiceRPS[choiceIndex];
     if (playerChoice === "r") {
         checkRockChoice();
     } else if (playerChoice === "s") {
@@ -17,6 +33,7 @@ do {
     } else {
         checkPaperChoice();
     }
+    alert("Wins: " + wins + "\n" + "Losses: "+losses+"\n"+"Ties: "+ ties);
     keepPlaying = confirm("Do you want to keep playing?");
 
 } while (keepPlaying);
